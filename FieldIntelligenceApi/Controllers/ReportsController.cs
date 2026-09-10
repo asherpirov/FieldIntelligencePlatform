9fde59a7f599b62c493147a51da40ac6e9ff09fd
Asher Pirov <Asherpirov@gmail.com>
2026-09-10 13:08:57 +0300

    feat: implement elasticsearch queries and statistics

--- FieldIntelligenceApi/Controllers/ReportsController.cs
+++ FieldIntelligenceApi/Controllers/ReportsController.cs
@@ -6,7 +6,7 @@ namespace FieldIntelligenceApi.Controllers;
 
 
 [ApiController]
-[Route("api/[controller]")]
+[Route("api/reports")]
 public class ReportsController : ControllerBase
 {
     private readonly IReportRepository _repository;
@@ -18,9 +18,35 @@ public class ReportsController : ControllerBase
 
     [HttpGet("search")]
     public async Task<ActionResult<IEnumerable<ReportDocument>>> SearchAsync(
-        [FromQuery] string? text)
+        [FromQuery] string? text,
+        [FromQuery] string? theater,
+        [FromQuery] string? sector,
+        [FromQuery] string? location,
+        [FromQuery] string[]? priorities,
+        [FromQuery] string? reportType,
+        [FromQuery] DateTime? from,
+        [FromQuery] DateTime? to)
     {
-        return Ok(await _repository.SearchAsync(text));
+        return Ok(await _repository.SearchAsync(text, theater, sector, location, priorities, reportType, from, to));
+    }
+
+    [HttpGet]
+    public async Task<ActionResult<IEnumerable<ReportDocument>>> GetReportsWithlocationAsync(
+       [FromQuery] string? theater,
+       [FromQuery] string? sector,
+       [FromQuery] string? location,
+       [FromQuery] string[]? priorities,
+       [FromQuery] DateTime? from,
+       [FromQuery] DateTime? to)
+    {
+        return Ok(await _repository.GetReportsAsync(theater, sector, location, priorities, from, to));
+    }
+
+    [HttpGet("statistics")]
+    public async Task<ActionResult<StatisticsResponseDto>> GetStatistics()
+    {
+        var statistics = await _repository.GetStatisticsAsync();
+        return Ok(statistics);
     }
 
 }
--- FieldIntelligenceApi/Repositories/IReportRepository.cs
+++ FieldIntelligenceApi/Repositories/IReportRepository.cs
@@ -4,5 +4,15 @@ namespace FieldIntelligenceApi.Repositories;
 
 public interface IReportRepository
 {
-    public Task<IEnumerable<ReportDocument>> SearchAsync(string? text);
+    public Task<IEnumerable<ReportDocument>> SearchAsync(string? text,
+    string? theater, string? sector, string? location,
+        string[]? priorities, string? reportType, DateTime? from, DateTime? to);
+    public Task<IEnumerable<ReportDocument>> GetReportsWithSubjectAsync(string subjectId);
+
+    public Task<IEnumerable<ReportDocument>> GetReportsAsync(
+        string? theater, string? sector, string? location,
+        string[]? priorities, DateTime? from, DateTime? to);
+
+    public Task<StatisticsResponseDto> GetStatisticsAsync();
+
 }
